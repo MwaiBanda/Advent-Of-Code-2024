10a14fa08fdd058a5074444b914150116b6c4f31
Mwai Banda <mwai.banda@cbn.org>
2024-12-01 00:27:58 -0600

    Completed day 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,21 +1,43 @@
+import kotlin.math.abs
+
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
+    fun part1(): Int {
+        val input = readInput("Day01")
+        val firstList = mutableListOf<Int>()
+        val secondList = mutableListOf<Int>()
+        input.forEach {
+            firstList.add(it.split(" ").first().trim().toInt())
+            secondList.add(it.split(" ").last().trim().toInt())
+        }
+        firstList.sort()
+        secondList.sort()
+
+        val differences = firstList.mapIndexed { index, i ->
+            abs(i - secondList[index])
+        }
+        return differences.reduce { acc, i -> acc + i }
     }
-
-    fun part2(input: List<String>): Int {
-        return input.size
+    fun part2(): Int {
+        val input = readInput("Day01")
+        val counts = mutableMapOf<Int, Int>()
+        val firstList = mutableListOf<Int>()
+        val secondList = mutableListOf<Int>()
+        input.forEach {
+            counts.putIfAbsent(it.split(" ").first().trim().toInt(), 0)
+            firstList.add(it.split(" ").first().trim().toInt())
+            secondList.add(it.split(" ").last().trim().toInt())
+        }
+        firstList.forEach { key ->
+            secondList.forEach {
+                if (counts.containsKey(it) && key == it) {
+                    counts[it] = (counts[it] ?: 0) + 1
+                }
+            }
+        }
+        return counts.map {
+            it.key * it.value
+        }.reduce { acc, i ->  acc + i }
     }
-
-    // Test if implementation meets criteria from the description, like:
-    check(part1(listOf("test_input")) == 1)
-
-    // Or read a large test input from the `src/Day01_test.txt` file:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
-
-    // Read the input from the `src/Day01.txt` file.
-    val input = readInput("Day01")
-    part1(input).println()
-    part2(input).println()
+    part1().println()
+    part2().println()
 }
